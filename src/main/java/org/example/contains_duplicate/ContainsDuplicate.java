15163a792f4e29660d4acf5cae9a0dd0e096f285
tr33p <whereareyo@yahoo.com>
2023-01-06 15:07:56 +0300

    contains duplicate

--- src/main/java/org/example/contains_duplicate/ContainsDuplicate.java
+++ src/main/java/org/example/contains_duplicate/ContainsDuplicate.java
@@ -2,6 +2,7 @@ package org.example.contains_duplicate;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
@@ -12,5 +13,16 @@ public class ContainsDuplicate {
 
     public static <T> boolean containsDuplicate(List<T> elems) {
         return new HashSet<>(elems).size() != elems.size();
+        // Or alternative
+/*        Set<T> seen = new HashSet<>();
+
+        for (T e : elems) {
+            if (seen.contains(e)) {
+                return true;
+            }
+            seen.add(e);
+        }
+
+        return false;*/
     }
 }
