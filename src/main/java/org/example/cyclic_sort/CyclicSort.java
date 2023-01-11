01b024bdaacc45392c7bdeee3dd4391640161de7
tre3p <whereareyo@yahoo.com>
2023-01-11 20:47:38 +0300

    cyclic sort main method removed

--- src/main/java/org/example/cyclic_sort/CyclicSort.java
+++ src/main/java/org/example/cyclic_sort/CyclicSort.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CyclicSort {
-    public static void main(String[] args) {
-        System.out.println(cyclicSort(new int[]{4,3,2,7,8,2,3,1}));
-    }
-
     public static List<Integer> cyclicSort(int[] nums) {
         int i = 0;
         while (i < nums.length) {
