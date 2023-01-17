5c6a6a091d61d57d48204a93a3310bd2125b5421
tre3p <whereareyo@yahoo.com>
2023-01-17 22:47:57 +0300

    another one fix

--- src/main/java/org/example/maximum_subarray/MaximumSubarray.java
+++ src/main/java/org/example/maximum_subarray/MaximumSubarray.java
@@ -10,11 +10,8 @@ public class MaximumSubarray {
             int num = nums[i];
 
             /**
-             * Rewriting current sum depends on max sum.
-             */
-            currentSum = Math.max(currentSum + num, num);
-
-            /**
+             * currentSum - variable for storing sums of digits.
+             *
              * If we take current number and add it to current sum, and this would be
              * worse than current num - we should start to count from this num, and do not count
              * digits on the left side.
@@ -25,6 +22,11 @@ public class MaximumSubarray {
              *
              * -5 + 10 = -5
              * So would be better, if we will start counting from 10, and adding digits to this num.
+             */
+            currentSum = Math.max(currentSum + num, num);
+
+            /**
+             * Rewriting maxSum depends on max sum.
              */
             maxSum = Math.max(currentSum, maxSum);
         }
