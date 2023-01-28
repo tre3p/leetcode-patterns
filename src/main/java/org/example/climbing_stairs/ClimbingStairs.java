77b1298108ac0b67ca466c18704fed2220140c75
tr33p <whereareyo@yahoo.com>
2023-01-28 22:33:40 +0300

    remove main from climbing stairs add javadoc added

--- src/main/java/org/example/climbing_stairs/ClimbingStairs.java
+++ src/main/java/org/example/climbing_stairs/ClimbingStairs.java
@@ -1,12 +1,17 @@
 package org.example.climbing_stairs;
 
 public class ClimbingStairs {
-    public static void main(String[] args) {
-        System.out.println(climbingStairs(23));
-    }
 
-
+    /**
+     * Represents implementation of Fibonacci numbers, because to climb stair N - it's the same as
+     * climb to stair (n - 1) + (n - 2)
+     * @param n
+     * @return
+     */
     public static int climbingStairs(int n) {
+        /**
+         * Base case. First fibonacci number = 1
+         */
         if (n == 1) {
             return n;
         }
